b3d740c7c03f17d7c451b3d81f9d4b7fe500f428
Christian Blais <christ.blais@gmail.com>
2016-03-26 23:35:53 -0400

    Move to Heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Parcelify
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.application_url = "https://65c4288b.ngrok.io"
+    config.application_url = "https://parcelify.herokuapp.com"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
