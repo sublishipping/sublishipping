b42a4c8a00eaa6cb15e8203e9f23304901b9f48e
Christian Blais <christ.blais@gmail.com>
2016-09-18 20:36:02 -0400

    Rescue ClientError

--- app/jobs/shop_update_job.rb
+++ app/jobs/shop_update_job.rb
@@ -13,7 +13,7 @@ class ShopUpdateJob < ApplicationJob
 
       shop.save!
     end
-  rescue ActiveResource::ResourceNotFound
+  rescue ActiveResource::ResourceNotFound, ActiveResource::ClientError
     Rails.logger.warn("[#{self.class.name}] Shop not found shop_id=#{shop.id} shop_domain=#{shop_domain}")
   end
 end
