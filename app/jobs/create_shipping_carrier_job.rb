39459554b4a6fea2a947144d0ea525795c315ba0
Christian Blais <christ.blais@gmail.com>
2016-11-25 13:20:46 -0500

    Wrap ShippingCarrierJob within shopify session

--- app/jobs/create_shipping_carrier_job.rb
+++ app/jobs/create_shipping_carrier_job.rb
@@ -2,18 +2,20 @@ class CreateShippingCarrierJob < ApplicationJob
   def perform(shop_domain:)
     shop = Shop.find_by(shopify_domain: shop_domain)
 
-    ShopifyAPI::CarrierService.find(:all).each do |carrier_service|
-      carrier_service.destroy
+    shop.with_shopify_session do
+      ShopifyAPI::CarrierService.find(:all).each do |carrier_service|
+        carrier_service.destroy
+      end
+
+      carrier_service = ShopifyAPI::CarrierService.create(
+        name: "Parcelify",
+        callback_url: "#{Rails.configuration.application_url}/callback/#{shop.id}.json",
+        format: "json",
+        service_discovery: true
+      )
+
+      shop.update_attribute(:shipping_carrier_id, carrier_service.id)
     end
-
-    carrier_service = ShopifyAPI::CarrierService.create(
-      name: "Parcelify",
-      callback_url: "#{Rails.configuration.application_url}/callback/#{shop.id}.json",
-      format: "json",
-      service_discovery: true
-    )
-
-    shop.update_attribute(:shipping_carrier_id, carrier_service.id)
   ensure
     unless shop.shipping_carrier_created?
       shop.update_attribute(:shipping_carrier_id, 0)
