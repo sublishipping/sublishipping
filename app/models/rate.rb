afe1f49ee74885d3bae2d023f3a33682e13a8846
Christian Blais <christ.blais@gmail.com>
2016-11-28 12:00:48 -0500

    Bump price limits for Chilean pesos

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -5,12 +5,12 @@ class Rate < ActiveRecord::Base
   has_many :product_specific_prices, dependent: :destroy
 
   validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }
-  validates :price_weight_modifier, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }
-  validates :min_price, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }, allow_nil: true
-  validates :max_price, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }, allow_nil: true
-  validates :min_grams, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }, allow_nil: true
-  validates :max_grams, numericality: { greater_than_or_equal_to: 0, less_than: 10_000_000 }, allow_nil: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }
+  validates :price_weight_modifier, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }
+  validates :min_price, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }, allow_nil: true
+  validates :max_price, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }, allow_nil: true
+  validates :min_grams, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }, allow_nil: true
+  validates :max_grams, numericality: { greater_than_or_equal_to: 0, less_than: 999_999_999 }, allow_nil: true
 
   accepts_nested_attributes_for :conditions, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :product_specific_prices, allow_destroy: true, reject_if: :all_blank
